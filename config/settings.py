caf5f2b471afefb39c9670cc49826c9fb04b48b3
Amine <medm22886@gmail.com>
2026-05-21 11:38:04 +0100

    configure cors for vercel deployment

--- config/settings.py
+++ config/settings.py
@@ -71,6 +71,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
 
 # CSRF Trusted Origins for production HTTPS requests
 raw_csrf = os.environ.get('CSRF_TRUSTED_ORIGINS', '')
